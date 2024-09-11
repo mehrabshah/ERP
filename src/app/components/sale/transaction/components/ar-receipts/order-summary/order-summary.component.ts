0ec1d3287e0b0d4fa4b4895d686ea5a683a2a587
Mehrab-developer <110405427+Mehrab-developer@users.noreply.github.com>
2024-09-12 02:18:25 +0500

    responsive design

--- src/app/components/sale/transaction/components/ar-receipts/order-summary/order-summary.component.ts
+++ src/app/components/sale/transaction/components/ar-receipts/order-summary/order-summary.component.ts
@@ -1,19 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-order-summary',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.css'
 })
 export class OrderSummaryComponent {
   @Input() summaryData: any ;
+  
+  isEditing: boolean = false;
 
   constructor() { }
   ngOnInit(): void {
  
+  }
+
+  toggleEdit() {
+    this.isEditing = !this.isEditing; 
   }
 
 }
