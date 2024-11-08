33d72d762ee95a2bbb9cf08729d9842a74ca8ed7
Mehrab-developer <mehrabalishah52@gmail.com>
2024-11-08 14:40:51 +0500

    Sales Return folder

---
+++ src/app/components/sale/transaction/components/sales-return/sales-return.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-return',
+  standalone: true,
+  imports: [],
+  templateUrl: './sales-return.component.html',
+  styleUrl: './sales-return.component.css'
+})
+export class SalesReturnComponent {
+
+}
